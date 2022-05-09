4960cca251422bf245cca49ad83b0d083879a815
asia <asia_jcrew96@yahoo.com>
2022-05-09 15:40:51 -0500

    added CRUD routes for posts and added Vote model

--- models/index.js
+++ models/index.js
@@ -1,2 +1,44 @@
 const User= require('./User');
-module.exports= { User };
+const Post= require('./Post');
+const Votes= require('./Vote');
+
+// A user have many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// Posts belong to User
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+  
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+  
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports= { User, Post, Vote };
